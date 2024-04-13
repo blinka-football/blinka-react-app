786c44ec8dfbd4443638e19421c2b128adfd8ad4
Prathit <prathit.shivade@gmail.com>
2024-04-13 17:11:52 +0100

    blinka:0.0.8: Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer col-sm-12">
+            <footer>
+                <p>FOR MORE DETAILS ABOUT OUR GAME OR FOR ANY ENQUIRIES, PLEASE FEEL FREE TO CONTACT US.</p>
+                <p> OUR DEDICATED TEAM IS HERE TO ASSIST YOU WITH ANY QUESTIONS YOU MAY HAVE. WE VALUE YOUR FEEDBACK AND ARE COMMITTED TO PROVIDING THE BEST EXPERIENCE POSSIBLE.</p>
+                <br></br>
+                <p className='email'>YOU CAN REACH US VIA EMAIL TO <a href="mailto:askblinka@gmail.com">NOOJ</a></p>
+                <br></br>
+                <p>THANK YOU FOR YOUR INTEREST IN OUR GAME. WE LOOK FORWARD TO HEARING FROM YOU!</p>
+                <br></br>
+                <p>COPYRIGHT &copy; 2024</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
