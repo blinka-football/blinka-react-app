7e80709670b51470d86bde8c5318b662a06b944f
Prathit <prathit.shivade@gmail.com>
2024-05-18 14:46:49 +0100

    blinka:0.0.13: Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
                 <p>FOR MORE DETAILS ABOUT OUR GAME OR FOR ANY ENQUIRIES, PLEASE FEEL FREE TO CONTACT US.</p>
                 <p> OUR DEDICATED TEAM IS HERE TO ASSIST YOU WITH ANY QUESTIONS YOU MAY HAVE. WE VALUE YOUR FEEDBACK AND ARE COMMITTED TO PROVIDING THE BEST EXPERIENCE POSSIBLE.</p>
                 <br></br>
-                <p className='email'>YOU CAN REACH US VIA EMAIL TO <a href="mailto:askblinka@gmail.com">NOOJ</a></p>
+                <p className='email'>YOU CAN REACH US VIA EMAIL AT <a href="mailto:askblinka@gmail.com">BLINKA</a></p>
                 <br></br>
                 <p>THANK YOU FOR YOUR INTEREST IN OUR GAME. WE LOOK FORWARD TO HEARING FROM YOU!</p>
                 <br></br>
